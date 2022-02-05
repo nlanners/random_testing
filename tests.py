9514ef344eaeec239d916a50fb408d2876d1de69
Nick Lanners <lannersn@oregonstate.edu>
2022-02-05 15:01:55 -0700

    fixed gen_check_bit

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 import string
 import random
+from credit_card_validator import credit_card_validator
 
 
 class TestCase(unittest.TestCase):
@@ -20,21 +21,24 @@ def gen_check_bit(num):
     modified_num = []
 
     # double odd indexes
-    for digit in num:
-        if digit % 2 != 0:
-            new_digit = digit * 2
+    for index in range(len(num)):
+        if index % 2 != 0:
+            new_digit = num[index] * 2
 
             # sum digits of new values
             if new_digit >= 10:
                 new_digit = 1 + (new_digit - 10)
             modified_num.append(new_digit)
         else:
-            modified_num.append(digit)
+            modified_num.append(num[index])
 
     # sum all digits
     digit_sum = sum(modified_num)
 
     # calculate check digit
-    check_bit = ((10 - digit_sum % 10) % 10)
+    check_bit = ((10 - (digit_sum % 10)) % 10)
 
     return check_bit
+
+if __name__ == '__main__':
+    unittest.main()
